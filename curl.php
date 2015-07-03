32d14bc1dd7b8995477062fa4fdc7b9e8af942f0
Hill <hill@kimo.com>
2015-07-03 14:30:24 +0400

    change sytle

--- curl.php
+++ curl.php
@@ -1,30 +1,27 @@
 <?php
 namespace PMVC\PlugIn\curl;
 
-\PMVC\l(__DIR__.'/curl_helper.php');
+\PMVC\l(__DIR__.'/src/curl_helper.php');
 
 ${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\curl';
 
 class curl extends \PMVC\PlugIn
 {
-
-    function init()
+    public function init()
     {
         $this->setDefaultAlias(new \Multi_Curl_Helper());
     }
 
 
 
-    function get($url=null,$function=null)
+    public function get($url=null, $function=null)
     {
-        if(!is_callable($function)){
-            return parent::get($path,$function);
+        if (!is_callable($function)) {
+            return parent::get($path, $function);
         }
         $oCurl = new \Curl_Helper();
         $oCurl->set_options($url);
-        $this->add($oCurl,null,$function);
+        $this->add($oCurl, null, $function);
         return $oCurl;
     }
-
 }
-?>
