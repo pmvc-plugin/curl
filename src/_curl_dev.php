445d4ca240b9d673d0e106cdf8ee188377397730
Hill <hill@kimo.com>
2022-07-06 22:59:08 +0000

    fixed syntax failed

--- src/_curl_dev.php
+++ src/_curl_dev.php
@@ -2,32 +2,28 @@
 
 namespace PMVC\PlugIn\curl;
 
-${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\CurlDev';
+${_INIT_CONFIG}[_CLASS] = __NAMESPACE__ . '\CurlDev';
 
 class CurlDev
 {
     public function __invoke($r, $opts)
     {
-        $rinfo = (array)$r;
-        $options = $this->
-            caller->
-            opt_to_str()->
-            all($opts);
-        $url = \PMVC\plug('url')->
-            getUrl($opts[CURLOPT_URL]);
+        $rinfo = (array) $r;
+        $options = $this->caller->opt_to_str()->all($opts);
+        $url = \PMVC\plug('url')->getUrl($opts[CURLOPT_URL]);
         $arrUrl = \PMVC\get($url);
         $arrUrl['query'] = \PMVC\get($url->query);
         $body = $this->caller->body_dev($rinfo['body']);
         unset($rinfo['body'], $rinfo['info']);
         $result = [
-            '-url'    => (string)$url,
+            '-url' => (string) $url,
             'urlObj' => $arrUrl,
-            'option' => $options, 
-            'respond'=> $rinfo,
-            'body'   => $body,
-            'help'   => [
-              'trace' => 'get trace info'
-              'req'   => 'get request information', 
+            'option' => $options,
+            'respond' => $rinfo,
+            'body' => $body,
+            'help' => [
+                'trace' => 'get trace info',
+                'req' => 'get request information',
             ],
         ];
 
